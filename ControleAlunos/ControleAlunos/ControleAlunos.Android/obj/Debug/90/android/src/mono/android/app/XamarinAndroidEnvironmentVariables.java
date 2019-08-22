91c89969031b5144907ea39171e0e2550110bcdf
terceiro-darocha <terceiro-darocha@icatuseguros.com.br>
2019-08-22 18:45:50 -0300

    Projeto com template prism

---
+++ ControleAlunos/ControleAlunos/ControleAlunos.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "7a9e89c6-7d8a-43e1-b58d-5ff6a53b30c2",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "System.Net.Http.HttpClientHandler, System.Net.Http",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep",
+
+	};
+}
